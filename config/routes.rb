a33e5cc211d64ee18d62c29f8c4ae33260f0dac5
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2023-11-03 15:00:02 +0200

    create user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users, path: '', path_names: {
-    sign_in: 'login',
-    sign_out: 'logout',
-    sign_up: 'register'
-  }
+  # devise_for :users, path: '', path_names: {
+  #   sign_in: 'login',
+  #   sign_out: 'logout',
+  #   sign_up: 'register'
+  # }
   
 
   # Defines the root path route ("/")
