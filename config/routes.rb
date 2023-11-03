46f42948defca2963af7df749fead1204c86d7ce
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2023-11-03 14:08:07 +0200

    devise:views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    sign_up: 'register'
+  }
+  
 
   # Defines the root path route ("/")
   # root "articles#index"
