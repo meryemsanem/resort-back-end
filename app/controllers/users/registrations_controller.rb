3a7294cfe3707efa69492d531ba1e9b793d2662a
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2023-11-04 16:09:12 +0200

    fix bug 2

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,22 +1,18 @@
-# frozen_string_literal: true
-
 class Users::RegistrationsController < Devise::RegistrationsController
   include RackSessionsFix
   respond_to :json
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
 
   private
 
-  def respond_with(resource, _opts = {})
+  def respond_with(current_user, _opts = {})
     if resource.persisted?
       render json: {
-        status: { code: 200, message: 'Signed up sucessfully.' },
-        data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
+        status: { code: 200, message: 'Signed up successfully.' },
+        data: UserSerializer.new(current_user).serializable_hash[:data][:attributes]
       }
     else
       render json: {
-        status: { message: "User couldn't be created successfully. #{resource.errors.full_messages.to_sentence}" }
+        status: { message: "User couldn't be created successfully. #{current_user.errors.full_messages.to_sentence}" }
       }, status: :unprocessable_entity
     end
   end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,8 +1,4 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :email, :created_at
-
-  attribute :created_date do |user|
-    user && user.created_at.strftime('%d/%m/%Y')
-  end
+  attributes :id, :email, :name
 end
