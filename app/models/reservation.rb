22ee226fc31762184ad2eeec686afd2b971d684f
meryemsanem <meryemsanemkucukbayram@gmail.com>
2023-11-06 17:20:11 +0300

    Add validations to reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,9 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :destination
+
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :user, presence: true
+  validates :destination, presence: true
 end
