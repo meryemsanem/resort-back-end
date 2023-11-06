86dca8796d79eddf0c676ba74463064903b23519
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2023-11-06 17:48:42 +0200

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -51,5 +51,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_06_135647) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "reservations", "destinations"
   add_foreign_key "reservations", "users"
 end
