1233f4c8bf25367bf257ae4428d9fc07e20114c2
Yusuf Sholotan <sholotanyusuf14@gmail.com>
2023-11-06 14:21:40 +0100

    Create resevations table

--- db/migrate/20231106130824_create_destinations.rb
+++ db/migrate/20231106130824_create_destinations.rb
@@ -5,7 +5,7 @@ class CreateDestinations < ActiveRecord::Migration[7.0]
       t.string :city_name
       t.numeric :fee
       t.text :description
-      t.string : descriptiont.string :image_url
+      t.string :image_url
       t.belongs_to :user, index: true, foreign_key:true
 
       t.timestamps
