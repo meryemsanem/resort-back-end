af862fcf9a22293fa40224ad6e573c636da0812b
meryemsanem <meryemsanemkucukbayram@gmail.com>
2023-11-09 01:21:30 +0300

    Update user and session controllers

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -5,7 +5,7 @@ class Users::SessionsController < Devise::SessionsController
 
   private
 
-  def respond_with(resource, _opts = {})
+   def respond_with(resource, _opts = {})
     render json: {
       status: { code: 200, message: 'Logged in sucessfully.' },
       data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
@@ -16,7 +16,7 @@ class Users::SessionsController < Devise::SessionsController
     if current_user
       render json: {
         status: 200,
-        message: 'logged out successfully'
+        message: 'Logged out successfully'
       }, status: :ok
     else
       render json: {
