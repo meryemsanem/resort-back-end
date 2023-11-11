5d05b980123ac28e74ad0fb1894dceb3e7b6fd95
meryemsanem <meryemsanemkucukbayram@gmail.com>
2023-11-12 02:26:37 +0300

    Fix linter errors

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -5,7 +5,7 @@ class Users::SessionsController < Devise::SessionsController
 
   private
 
-   def respond_with(resource, _opts = {})
+  def respond_with(resource, _opts = {})
     render json: {
       status: { code: 200, message: 'Logged in sucessfully.' },
       data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
