ba95d3fa71e31f81f88e4554cd46a670b02af13e
meryemsanem <meryemsanemkucukbayram@gmail.com>
2023-11-12 02:45:09 +0300

    Update routes to include current_user mapping

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
+
 Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
@@ -14,6 +15,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users do
+        get 'users/current_user', to: 'users#current_user'
         resources :reservations
       end
       resources :destinations
