79ec010d574ee8781aa5a9b659e9f91b6a1b5b91
meryemsanem <meryemsanemkucukbayram@gmail.com>
2023-11-12 05:10:03 +0300

    Update index and create actions with destination info for creating reservations

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,20 +1,41 @@
 class Api::V1::ReservationsController < ApplicationController
   before_action :set_reservation, only: %i[show update destroy]
 
-  def index
-    @reservations = Reservation.where(user_id: params[:user_id])
-    render json: @reservations
+ def index
+  @reservations = Reservation.all.includes(:destination)
+
+  data = @reservations.map do |reservation|
+    {
+      id: reservation.id,
+      destination_id: reservation.destination.id,
+      start_date: reservation.start_date,
+      end_date: reservation.end_date,
+      user: reservation.user.name,
+      user_id: reservation.user.id,
+      destination: {
+        name: reservation.destination.name,
+        city_name: reservation.destination.city_name,
+      }
+    }
   end
+  render json: data
+end
 
   def show
     render json: @reservation
   end
 
   def create
-    @reservation = Reservation.new(reservation_params)
+    destination = Destination.find(params[:destination_id])
+
+    @reservation = current_user.reservations.build(
+      destination: destination,
+      start_date: params[:start_date],
+      end_date: params[:end_date]
+    )
 
     if @reservation.save
-      render json: @reservation, status: :created
+      render json: @reservation, include: { destination: { only: [:name, :city_name] } }, status: :created
     else
       render json: @reservation.errors, status: :unprocessable_entity
     end
