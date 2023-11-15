f94043734704d44c882c99e4049ae5ceb600ac53
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2023-11-16 00:48:51 +0200

    FIX rails db:migrate error

--- db/migrate/20231115224326_create_reservations.rb
+++ db/migrate/20231115224326_create_reservations.rb
@@ -5,7 +5,6 @@ class CreateReservations < ActiveRecord::Migration[7.0]
       t.references :destination, null: false, foreign_key: true
       t.date :start_date
       t.date :end_date
-      t.references :destination, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_06_135647) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_15_224326) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,5 +51,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_06_135647) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "reservations", "destinations"
   add_foreign_key "reservations", "users"
 end
