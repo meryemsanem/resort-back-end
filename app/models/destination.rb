46ea7f289085f5fee9b76c887fe683ae20fd5191
Yusuf Sholotan <sholotanyusuf14@gmail.com>
2023-11-16 10:45:10 +0100

    Update image_url property.

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -4,6 +4,6 @@ class Destination < ApplicationRecord
   validates :name, presence: true, length: { minimum: 3, maximum: 30 }
   validates :city_name, presence: true, length: { minimum: 3, maximum: 30 }
   validates :description, presence: true, length: { minimum: 3, maximum: 250 }
-  validates :image_url, presence: true, length: { minimum: 3, maximum: 150 }
+  validates :image_url, presence: true, length: { minimum: 3 }
   validates :fee, presence: true, length: { minimum: 1, maximum: 10 }
 end
