9e501ec672f7da894d1b73d67fc3f8018093a8d5
Yusuf Sholotan <sholotanyusuf14@gmail.com>
2023-11-16 10:43:57 +0100

    Update maximum length of description

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -3,7 +3,7 @@ class Destination < ApplicationRecord
 
   validates :name, presence: true, length: { minimum: 3, maximum: 30 }
   validates :city_name, presence: true, length: { minimum: 3, maximum: 20 }
-  validates :description, presence: true, length: { minimum: 3, maximum: 25 }
+  validates :description, presence: true, length: { minimum: 3, maximum: 250 }
   validates :image_url, presence: true, length: { minimum: 3, maximum: 150 }
   validates :fee, presence: true, length: { minimum: 1, maximum: 10 }
 end
